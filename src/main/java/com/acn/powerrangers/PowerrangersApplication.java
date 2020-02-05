b70b82c42ddeca97f85def2cf8080a1906f93af2
Turaboev <hakimjon.turaboev@accenture.com>
2020-02-05 09:07:55 +0100

    ein Projekt von powerrangers initialisiert

---
+++ src/main/java/com/acn/powerrangers/PowerrangersApplication.java
@@ -0,0 +1,13 @@
+package com.acn.powerrangers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerrangersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerrangersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acn/powerrangers/PowerrangersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acn.powerrangers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerrangersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
